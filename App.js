4bbef5eb4fe36169ea5de8863adeb72d95a124df
surya <suryamunaga89@gmail.com>
2024-02-12 16:41:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, Image, ScrollView, TextInput, StyleSheet, Button} from 'react-native';
+
 
 export default function App() {
+  const [cnt,setCnt]=useState(0)
+  const fnIncrement=()=>{
+    setCnt(cnt+1)
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  <ScrollView>
+    <h1 style={styles.cr}>{cnt}</h1>
+    <Button
+  onPress={fnIncrement}
+  title="surya loves gayathri"
+  color="#841584"
+  
+/>
+  <Text>Some text</Text>
+  <View>
+    <Text>Some more text</Text>
+    <Image
+      source={{
+        uri: "https://d2mk45aasx86xg.cloudfront.net/Best_React_Native_tools_206b76b4b7.webp",
+      }}
+      style={{width: 200, height: 200}}
+    />
+  </View>
+  <TextInput
+    style={{
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+    }}
+    defaultValue="You can type in me"
+  />
+</ScrollView>
+)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ cr:{
+  color:"orange"
+ }
 });
